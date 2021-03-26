7f187ab3d27d955993bc6d352f327b75b2462af9
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-03-26 01:54:12 -0700

    LogInScreen style

--- App.jsx
+++ App.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen'
+import LogInScreen from './src/screens/LogInScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
+    <LogInScreen />
   );
 }
